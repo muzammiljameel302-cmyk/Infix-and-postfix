4a73e50bc09468257b70ee5074cbf1d69457ebc4
Fatima Razzaq <fatimaabdulrazzaq03@gmail.com>
2026-06-16 21:12:52 +0500

    Implement runRule function to evaluate device rules

--- fatima_evaluation.cpp
+++ fatima_evaluation.cpp
@@ -1,1 +1,16 @@
+// Run one rule and turn the device ON or OFF
+void runRule(string device, string rule, map<string,double>& sensors) {
+    vector<string> words = splitWords(rule);
+    vector<string> postfix = toPostfix(words);
 
+    cout << "\n--- " << device << " ---\n";
+    cout << "Rule (Infix): " << rule << "\n";
+    cout << "Postfix : ";
+    for (string w : postfix) cout << w << " ";
+    cout << "\n";
+
+    bool on = evaluate(postfix, sensors);
+
+    cout << "Result : " << (on ? "TRUE" : "FALSE") << "\n";
+    cout << device << " is turned " << (on ? "ON" : "OFF") << "\n";
+}
